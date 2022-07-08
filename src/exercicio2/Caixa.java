12880ce606adb5fbaf025cecc0969cccde8a798f
davispacheco <davipacheco1996@gmail.com>
2022-07-08 17:08:16 -0300

    Criar classe Caixa, com os atributos saldo, valor, lancamentosCredito e lancamentosDebito, bem como os métodos credito e debito.

---
+++ src/exercicio2/Caixa.java
@@ -0,0 +1,71 @@
+package src.exercicio2;
+
+public class Caixa {
+    private double saldo;
+    private double valor;
+    private int lancamentosCredito;
+    private int lancamentosDebito;
+
+    public Caixa() {
+        this.saldo = 1000.0;
+        this.valor = valor;
+        this.lancamentosCredito = 0;
+        this.lancamentosDebito = 0;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getLancamentosCredito() {
+        return lancamentosCredito;
+    }
+
+    public void setLancamentosCredito(int lancamentosCredito) {
+        this.lancamentosCredito = lancamentosCredito;
+    }
+
+    public int getLancamentosDebito() {
+        return lancamentosDebito;
+    }
+
+    public void setLancamentosDebito(int lancamentosDebito) {
+        this.lancamentosDebito = lancamentosDebito;
+    }
+
+    public boolean credito(double valor) {
+        boolean sucesso;
+        if (valor <= 0) {
+            sucesso = false;
+        } else {
+            saldo += valor;
+            lancamentosCredito++;
+            sucesso = true;
+        }
+        return sucesso;
+    }
+
+    public boolean debito(double valor) {
+        boolean sucesso;
+        if (valor <= 0 && valor > saldo ) {
+            sucesso = false;
+        } else {
+            saldo -= valor;
+            lancamentosDebito ++;
+            sucesso = true;
+        }
+        return sucesso;
+    }
+}
